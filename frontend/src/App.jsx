eadd615a4fedbb4c9512f703f9b679b66d8ce598
Ayush <ayush.ak5555@gmail.com>
2021-10-06 16:44:35 +0530

    feat:Added carasouel to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Carasouel from './components/carasouel/Carasouel';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header isOpen={isOpen} setOpen={setOpen} />
       <Sidebar isOpen={isOpen} setOpen={setOpen} />
+      <Carasouel/>
     </div>
   );
 }
---
+++ frontend/src/components/carasouel/Carasouel.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import "./Carasouel.css";
+function Carasouel() {
+
+   
+
+
+  return (
+    <Carousel
+      autoPlay="true"
+    //   centerMode="true"
+      infiniteLoop="true"
+      showArrows="false"
+      showThumbs="false"
+      showStatus="false"
+      showIndicators="false"
+    >
+      <div className="carasoul-content">
+          
+        <p>Handpicked cars as good as new</p>
+      </div>
+
+      <div className="carasoul-content">
+        <p>7 days trial or full refund</p>
+      </div>
+
+      <div className="carasoul-content">
+        <p>6 months warranty on all cars</p>
+      </div>
+      
+    </Carousel>
+  );
+}
+
+export default Carasouel;
